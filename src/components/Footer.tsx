91b28e1dca0c29613ff27ece4b2686d1da96c14b
stupidbox <whdals6831@gmail.com>
2025-12-07 18:20:10 +0900

    style : 출처 텍스트 띄어쓰기 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <div className="fixed bottom-0 w-full bg-black/60 text-white px-2 py-1 text-xs text-center z-10">
       <a
         href="https://skfb.ly/pDDH6"
-        className="text-blue-500 underline"
+        className="text-blue-500 underline mr-1"
         target="_blank"
       >
         Isle of Pigs Red
@@ -11,7 +11,7 @@ export function Footer() {
       by OrangeSauceu is licensed under
       <a
         href="http://creativecommons.org/licenses/by/4.0/"
-        className="text-blue-500 underline"
+        className="text-blue-500 underline ml-1"
         target="_blank"
       >
         Creative Commons Attribution
