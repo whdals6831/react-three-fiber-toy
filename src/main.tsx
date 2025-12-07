0270249a3cddf546cb41dd2b02ce0b720eee81ad
stupidbox <whdals6831@gmail.com>
2025-12-07 21:41:30 +0900

    style : antd 다크모드 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from '@/app/App';
+import { ConfigProvider, theme } from 'antd';
 
 const root = document.getElementById('root');
 if (!root) throw Error('root elements not exist!');
 
 createRoot(root).render(
   // <StrictMode>
-  <App />,
+  <ConfigProvider
+    theme={{
+      algorithm: theme.darkAlgorithm,
+    }}
+  >
+    <App />,
+  </ConfigProvider>,
   // </StrictMode>,
 );
